0347e25c2d394d91f12c63098d948ad7d558ab1a
Aryama10 <aryamatripathi10@gmail.com>
2026-07-06 22:09:09 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,18 @@
 function Navbar() {
   return (
-    <nav
-      style={{
-        backgroundColor: "#212529",
-        padding: "20px 50px",
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center"
-      }}
-    >
-      <h2 style={{ color: "white" }}>MyPortfolio</h2>
+    <nav className="navbar-custom">
+      <h2 className="logo">MyPortfolio</h2>
 
-      <ul
-        style={{
-          display: "flex",
-          listStyle: "none",
-          gap: "30px"
-        }}
-      >
-        <li><a href="#" style={{ color: "white", textDecoration: "none" }}>Home</a></li>
-        <li><a href="#" style={{ color: "white", textDecoration: "none" }}>About</a></li>
-        <li><a href="#" style={{ color: "white", textDecoration: "none" }}>Projects</a></li>
-        <li><a href="#" style={{ color: "white", textDecoration: "none" }}>Certificates</a></li>
-        <li><a href="#" style={{ color: "white", textDecoration: "none" }}>Skills</a></li>
-        <li><a href="#" style={{ color: "white", textDecoration: "none" }}>Contact</a></li>
+      <ul className="nav-links">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#certificates">Certificates</a></li>
+        <li><a href="#skills">Skills</a></li>
+        <li><a href="#contact">Contact</a></li>
       </ul>
     </nav>
   )
 }
 
-export default Navbar
+export default Navbar;

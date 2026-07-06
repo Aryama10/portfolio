60319bce287e8cbc92dd401752889ddac3f8434b
Aryama10 <aryamatripathi10@gmail.com>
2026-07-06 22:24:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 import './App.css'
 import Navbar from './components/Navbar'
-import Home from'./components/Home'
+import Home from './components/Home'
 import About from './components/About'
 import Projects from './components/Projects'
 import Certificates from './components/Certificates'
 import Skills from './components/Skills'
 import Contact from './components/Contact'
+
 function App() {
   return (
     <>
-    <Navbar/>
-    <Home/>
-    <About/>
-    <Projects/>
-    <Certificates/>
-    <Skills/>
-    <Contact/>
+      <Navbar />
+      <Home />
+      <About />
+      <Projects />
+      <Certificates />
+      <Skills />
+      <Contact />
     </>
   )
 }
-export default App
+
+export default App;
